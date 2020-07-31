90f377e0ac1112cbb2a61162a6ea1f808628d6fe
g9i2b <henryroh2000@gmail.com>
2020-07-31 15:08:49 -0700

    lockout for tab-switching

--- quickswitch.py
+++ quickswitch.py
@@ -1,6 +1,13 @@
 import time
 import pynput
 import math
+import threading
+
+lock = False
+
+def unlock():
+    global lock
+    lock = False
 
 def getTime():
     return time.time()*1000
@@ -11,9 +18,12 @@ def distance(p1, p2):
     return math.sqrt(math.pow(x1-x2,2) + math.pow(y1-y2,2))
 
 def quickSwitch():
+    global lock
     with keyboard.pressed(pynput.keyboard.Key.alt):
-        keyboard.press(pynput.keyboard.Key.tab)
-        keyboard.release(pynput.keyboard.Key.tab)
+        if not lock:
+            keyboard.press(pynput.keyboard.Key.tab)
+            keyboard.release(pynput.keyboard.Key.tab)
+            threading.Timer(LOCK_DURATION, unlock)
 
 def on_move(x, y):
     global ppos, ptime
@@ -22,9 +32,10 @@ def on_move(x, y):
     if  ctime >=  (ptime + SAMPLERATE):
         if  distance(cpos, ppos) > THRESHOLD:
             quickSwitch()
-
         ppos = cpos
         ptime = ctime
+
+
 
 def on_click(x, y, button, pressed):
     if button == pynput.mouse.Button.middle and not pressed:
@@ -33,6 +44,7 @@ def on_click(x, y, button, pressed):
 #CONSTANTS
 THRESHOLD = 400 #pixels
 SAMPLERATE = 10 #ms
+LOCK_DURATION  = 0.5 #s
 
 keyboard = pynput.keyboard.Controller()
 mouse = pynput.mouse.Controller()
